5745ea3c1aa6f3d53cdb9ba38f46c98f3e21e0f6
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-10-13 03:08:21 +0100

    Socialite done

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
     public function redirect()
     {
-        // Code here
+        return Socialite::driver('github')->redirect();
     }
 
     public function authenticate()
     {
-        // Code here
+        $user = Socialite::driver('github')->user();
+
+        $user = User::updateOrCreate([
+            'github_id' => $user->id,
+        ], [
+            'github_id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'github_token' => $user->token,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/dashboard');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'github_id',
+        'github_token'
     ];
 
     /**
@@ -31,6 +33,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'github_token'
     ];
 
     /**
